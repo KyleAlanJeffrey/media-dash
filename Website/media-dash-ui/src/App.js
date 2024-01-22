dd235d1ff9cb3ca6b067fcf4ab43e85933b1f90b
kjeffery <kylejeffrey@stoutagtech.com>
2024-01-21 16:11:13 -0800

    feat: Add fetch

--- Website/media-dash-ui/src/App.js
+++ Website/media-dash-ui/src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+
+const PORT = 8080;
 
 function App() {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    fetch("http://localhost:" + PORT + "/currently_playing").then(response => {
+      response.json().then(data => {
+        console.log(data);
+        setData(data);
+      })
+    }
+    );
+  
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>{data}</h1>
       </header>
     </div>
   );
