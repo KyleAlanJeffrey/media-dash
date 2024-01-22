3be4f42d9b4030c444cb45d22d0014ebfda9d86b
kjeffery <kylejeffrey@stoutagtech.com>
2024-01-21 16:12:12 -0800

    add interval

--- Website/media-dash-ui/src/App.js
+++ Website/media-dash-ui/src/App.js
@@ -1,24 +1,26 @@
-import './App.css';
-import React, { useEffect, useState } from 'react';
+import "./App.css";
+import React, { useEffect, useState } from "react";
 
 const PORT = 8080;
 
 function App() {
   const [data, setData] = useState({});
   useEffect(() => {
-    fetch("http://localhost:" + PORT + "/currently_playing").then(response => {
-      response.json().then(data => {
-        console.log(data);
-        setData(data);
-      })
-    }
-    );
-  
+    setInterval(() => {
+      fetch("http://localhost:" + PORT + "/currently_playing").then(
+        (response) => {
+          response.json().then((data) => {
+            console.log(data);
+            setData(data);
+          });
+        }
+      );
+    });
   }, []);
   return (
     <div className="App">
       <header className="App-header">
-       <h1>{data}</h1>
+        <h1>{data}</h1>
       </header>
     </div>
   );
